d1f739290a6011635244ee622673bc151f0dfbee
Steven Douglas <steven@douglas-enterprises.com>
2017-09-05 20:35:31 -0700

    initial commit

---
+++ README.rst
@@ -0,0 +1,3 @@
+bapiclient - bhyve api client
+
+This is a client library to the bhyve api (bapi). It supports multiple bapi endpoints via environment variables.
---
+++ bapiclient/client.py
@@ -0,0 +1,62 @@
+#!/usr/local/bin/python
+
+import os
+import requests
+
+HOSTS = os.environ['HOSTS'].replace(' ', '').split(',')
+print $HOSTS
+def get_all_vms():
+    """ Returns a list of VM's that we can see """
+    vms = []
+    for host in HOSTS:
+        url = "%s/vm/" % host
+        r = requests.get(url)
+        vml = r.json()['vms']
+        for vm in vml:
+            vms.append(vm)
+    return vms
+
+def find_vm_host(vm_name):
+    """ Find the host that a vm resides on """
+    for host in HOSTS:
+        url = "%s/vm/" % host
+        r = requests.get(url)
+        vml = r.json()['vms']
+        if vm_name in vml:
+            return host
+    else:
+        raise IOError("VM %s doesn't exist" % vm_name)
+
+def get_vm_details(vm_name):
+    """ Return a json dump of everything we know about a VM """
+    host = find_vm_host(vm_name)
+    url = "%s/vm/%s/dump" % (host, vm_name)
+    r = requests.get(url)
+    rtrn = r.json()
+    rtrn['websocket_port'] = '10' + rtrn['fbuf_port'][1:]
+    return rtrn
+
+def vm_action(vm_name, action):
+    """ Run a particular action on a VM """
+    host = find_vm_host(vm_name)
+    url = "%s/vm/%s" %(host, vm_name)
+
+    # GET - status
+    if action == 'status':
+        r = requests.get(url)
+
+    elif action == 'delete':
+        r = requests.delete(url)
+        if r.status_code != 202:
+            raise
+        else:
+            return
+
+    # POST - start stop restart
+    elif action == 'start' or action == 'stop' or action == 'restart':
+        r = requests.post(url, json={'action': action})
+    else:
+        raise
+    
+    return r.json()
+
